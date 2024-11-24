4ac3f26594c079183d1e7d8c5113a1da0615fb48
kench <77647366+kench72@users.noreply.github.com>
2024-11-24 18:36:54 +0900

    SecurityContextRepositoryとセッション固定化攻撃への対策

--- src/main/java/org/example/loan/config/SecurityConfig.java
+++ src/main/java/org/example/loan/config/SecurityConfig.java
@@ -12,34 +12,61 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.session.ChangeSessionIdAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.security.web.context.SecurityContextRepository;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
-        @Bean
-        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-            http
-                .csrf(csrf-> csrf.ignoringRequestMatchers("/login"))
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+            HttpSecurity http
+            , SecurityContextRepository securityContextRepository
+            , SessionAuthenticationStrategy sessionAuthenticationStrategy
+    ) throws Exception {
+        http
+                .csrf(csrf -> csrf.ignoringRequestMatchers("/login"))
                 .addFilterAt(
-                        new JsonUsernamePasswordAuthenticationFilter(),
+                        new JsonUsernamePasswordAuthenticationFilter(
+                                securityContextRepository,
+                                sessionAuthenticationStrategy
+                        ),
                         UsernamePasswordAuthenticationFilter.class
                 )
+                .securityContext(context -> context.securityContextRepository(securityContextRepository))
                 .authorizeHttpRequests((authorize) -> authorize
-                            .requestMatchers("/articles/**").permitAll()
-                            .anyRequest().authenticated()
-                    )
-                    //.httpBasic(Customizer.withDefaults()) basic認証は使わない
-                    .formLogin(Customizer.withDefaults());
+                        .requestMatchers("/articles/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+                //.httpBasic(Customizer.withDefaults()) basic認証は使わない
+                .formLogin(Customizer.withDefaults());
 
-            return http.build();
-        }
+        return http.build();
+    }
 
-        /// user情報を取得するサービス
-        /// user情報を持ってくるサービスをUserDetailsServiceという
-        @Bean
-        public UserDetailsService userDetailsService() {
-            UserDetails userDetails = User.withDefaultPasswordEncoder()
+    // SecurityContextRepositoryのBean登録
+    // HttpSessionSecurityContextRepositoryをSecurityContextRepositoryという型でBean登録する
+    @Bean
+    public SecurityContextRepository securityContextRepository() {
+        return new HttpSessionSecurityContextRepository();
+    }
+
+    // SessionAuthenticationStrategyのBean登録
+    // ChangeSessionIdAuthenticationStrategyをSessionAuthenticationStrategyという型でBean登録する
+    // Bean登録された型はsecurityFilterChainの引数に注入してくれます（=Bean取得）
+    @Bean
+    public SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new ChangeSessionIdAuthenticationStrategy();
+    }
+
+    /// user情報を取得するサービス
+    /// user情報を持ってくるサービスをUserDetailsServiceという
+    @Bean
+    public UserDetailsService userDetailsService() {
+        UserDetails userDetails = User.withDefaultPasswordEncoder()
                 .username("user")
                 .password("password")
                 .roles("USER")
